778f36e5b632ec99c0f60055a7476bcad9c0375c
Kolupsy <kolupsy@gmail.com>
2023-05-21 13:28:50 +0200

    add Cook asset
    with animation manager as an abstraction layer

---
+++ Assets/Scripts/CookAnimationManager.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CookAnimationManager : MonoBehaviour
+{
+
+    public Animator animator;
+    public Transform Egg;
+    public Transform TopShell;
+    public Transform BottomShell;
+
+    public void Idle()
+    {
+        animator.SetTrigger("Idle");
+    }
+    public void GetEgg()
+    {
+        animator.SetTrigger("Get");
+    }
+    public void LiftEgg()
+    {
+        animator.SetTrigger("Lift");
+    }
+    public void Smack(bool success)
+    {
+        animator.SetTrigger(success? "Smack" : "SmackTooHard");
+    }
+    public void Crack(bool success)
+    {
+        animator.SetBool("CrackSuccess", success);
+        animator.SetTrigger("Crack");
+    }
+    public void RaisePan()
+    {
+        animator.SetBool("ShowMenu", true);
+    }
+    public void LowerPan()
+    {
+        animator.SetBool("ShowMenu", false);
+    }
+
+    #if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new (1,1,.8f);
+        Gizmos.DrawWireSphere(Egg.position, .05f);
+    }
+    
+    #endif
+}
