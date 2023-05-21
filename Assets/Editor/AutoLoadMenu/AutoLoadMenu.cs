92c0c79639f72af97a96c22050695b28decb9066
alxyzz <93402865+alxyzz@users.noreply.github.com>
2023-05-21 12:23:21 +0200

    Update AutoLoadMenu.cs to stop loading mainmenu scene

--- Assets/Editor/AutoLoadMenu/AutoLoadMenu.cs
+++ Assets/Editor/AutoLoadMenu/AutoLoadMenu.cs
@@ -8,10 +8,10 @@ public class AutoLoadMenu
 {
     static AutoLoadMenu()
     {
-        var pathOfFirstScene = EditorBuildSettings.scenes[0].path;
-        var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(pathOfFirstScene);
-        EditorSceneManager.playModeStartScene = sceneAsset;
-        Debug.Log(pathOfFirstScene + " was set as default play mode scene");
+        //var pathOfFirstScene = EditorBuildSettings.scenes[0].path;
+        //var sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(pathOfFirstScene);
+        EditorSceneManager.playModeStartScene = null;
+        //Debug.Log(pathOfFirstScene + " was set as default play mode scene");
     }
 }
 #endif
